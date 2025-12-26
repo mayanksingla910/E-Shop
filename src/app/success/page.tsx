bdc2fd4c354439e6ddd9bd1bbc9a24f3b37a28bd
Mayank Singla <Singlamayank10@gmail.com>
2025-12-26 09:01:32 +0530

    fixed success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,35 +1,10 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import Confetti from "react-confetti";
+import { Suspense } from "react";
+import SuccessClient from "@/components/successClient";
 
-const SuccessPage = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const orderId = searchParams.get("orderId");
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!orderId) router.push("/");
-      else router.push("/orders/" + orderId);
-    }, 5000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [orderId, router]);
-
+export default function Page() {
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
-      <Confetti width={2000} height={1000} />
-      <h1 className="text-6xl text-green-700">Successful</h1>
-      <h2 className="text-xl font-medium">
-        We sent the invoice to your e-mail
-      </h2>
-      <h3 className="">You are being redirected to the order page...</h3>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuccessClient />
+    </Suspense>
   );
-};
-
-export default SuccessPage;
+}
